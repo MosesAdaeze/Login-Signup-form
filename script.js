f197f19f62ce7ae0b824d78882ada119120bb0f6
Kloss <108338208+MosesAdaeze@users.noreply.github.com>
2023-02-10 20:20:58 +0100

    signup

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function openloginpage() {
     document.getElementById('login-container').style.display = "flex"
 }
+
+function opensignuppage() {
+    document.getElementById('signup-container').style.display = "flex"
+}
